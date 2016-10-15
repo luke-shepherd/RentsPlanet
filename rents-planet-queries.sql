d015e74563d5d39e30e86ac6cf2b18752a72abd6
virajp691 <virajp691@gmail.com>
2016-10-15 16:26:09 -0700

    Added the basic sql files

---
+++ rents-planet-queries.sql
@@ -0,0 +1,5 @@
+INSERT INTO houses (address, rent landlord)
+VALUES ('123 Apple St. SF CA 4093083', 4500, 'John Doe');
+
+INSERT INTO houses (address, rent landlord)
+VALUES ('555 Some Ave. SC CA 40875', 6000, 'John Doe');
---
+++ start-server.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE rentsplanet;
+SHOW DATABASES;
+USE rentsplanet;
+CREATE TABLE houses (
+	address		VARCHAR(50),
+	rent 		INT,
+	landlord 	VARCHAR(25)
+	);
