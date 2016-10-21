195f4ec7a95e574da3c27edec1e604e5ffa12057
virajp691 <virajp691@gmail.com>
2016-10-21 15:18:11 -0700

    RentDAO now uses RentEntryBean objects

--- cmps115Project/RentsPlanet/src/main/java/com/dl/rentsplanet/RentDAO.java
+++ cmps115Project/RentsPlanet/src/main/java/com/dl/rentsplanet/RentDAO.java
@@ -8,51 +8,61 @@ import java.util.List;
 import java.util.Date;
 import java.util.Iterator;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.hibernate.SessionFactory;
+import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
 
 public class RentDAO {
     private static SessionFactory factory ;
- /*   public static void main(String[] args) {
-//        try {
-//            factory = new Configuration().configure().buildSessionFactory();
-//        }catch (Throwable ex) {
-//            System.err.println("!!!Failed to build session factory. " + ex);
-//            throw new ExceptionInInitializerError(ex);
-//        }
-        RentDAO m = new RentDAO();
-        //Integer id1 = m.addHouse("123 Mission st", 5000, "bob");
-        //Integer id2 = m.addHouse("576 Scjlsha st", 7000, "Smith");
-        //Integer id3 = m.addHouse("123 Mission st", 4000, "Joe");
-        //m.listHouses();
-    }
-    /* Method to CREATE a house in the database */
-    public Integer addHouse(RentEntryBean rentEntry){
-        try{
+    /*public static void main(String[] args) {
+        try {
             factory = new Configuration().configure().buildSessionFactory();
         }catch (Throwable ex) {
-            System.err.println("!!!Failed to build SessionFactory in addHouse() "+ex);
+            System.err.println("!!!Failed to build session factory. " + ex);
             throw new ExceptionInInitializerError(ex);
         }
+        RentDAO db = new RentDAO();
+        RentEntryBean h1 = new RentEntryBean();
+        h1.setAddress("123 First St"); h1.setRent("1000"); h1.setLandLord("Adam");
+
+        RentEntryBean h2 = new RentEntryBean();
+        h2.setAddress("234 Second St."); h2.setRent("2000"); h2.setLandLord("Billy");
+
+        db.addHouse(h1);
+        db.addHouse(h2);
+        //db.addHouse(h1);
+        db.listHouses();
+        db.deleteHouse(h1);
+        db.deleteHouse(h2);
+
+        System.exit(0);
+    }*/
+
+    /* Method to INSERT house in the database */
+    public Integer addHouse(RentEntryBean rentEntry) {
+        try {
+            factory = new Configuration().configure().buildSessionFactory();
+        }catch (Throwable ex) {
+            System.err.println("!!!Failed to build SessionFactory in addHouse()!!! "+ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+
         Session session = factory.openSession();
-        Transaction tx = null;
+        //Transaction tx = null;
         Integer houseID = null;
         try{
-            tx = session.beginTransaction();
+            session.beginTransaction();
             House house = new House(rentEntry.getAddress(), Integer.parseInt(rentEntry.getRent()), rentEntry.getLandLord());
             houseID = (Integer) session.save(house);
-            tx.commit();
+            session.getTransaction().commit();
         }catch (HibernateException e) {
-            if (tx!=null) tx.rollback();
+            System.err.println("!!!Failed to Insert House into DB" + e);
             e.printStackTrace();
         }finally {
             session.close();
         }
         return houseID;
     }
+
     /* Method to  READ all the houses */
     public List<RentEntryBean> listHouses(){
         try{
@@ -62,25 +72,18 @@ public class RentDAO {
             throw new ExceptionInInitializerError(ex);
         }
         Session session = factory.openSession();
-        Transaction tx = null;
     	List<RentEntryBean> house_list = new ArrayList<RentEntryBean>();
         try{
-            tx = session.beginTransaction();
-            List houses = session.createQuery("FROM House").list();
-            Iterator iter = houses.iterator();
-	        while(iter.hasNext()) {
-                House house = (House) iter.next();
-	            house_list.add(house.toRentEntryBean());
-               /* for (Iterator iterator =
-                 houses.iterator(); iterator.hasNext();) {*/
-               // House employee = (House) iterator.next();
-               // System.out.print("  Address: " + employee.getAddress());
-               // System.out.print("  Rent: " + employee.getRent());
-               // System.out.println("  Landlord: " + employee.getLandlord());
+            session.beginTransaction();
+            String hql = "from House";
+            List<House> result = session.createQuery(hql).list();
+            for (House h : result) {
+                house_list.add(h.toRentEntryBean());
+                System.out.println("Address: "+h.getAddress());             //DELETE WHEN NOT TESTING
             }
-            tx.commit();
-        }catch (HibernateException e) {
-            if (tx!=null) tx.rollback();
+            session.getTransaction().commit();
+            }catch (HibernateException e) {
+            System.err.println("!!!FAILED to List Houses" + e);
             e.printStackTrace();
         }finally {
             session.close();
@@ -112,7 +115,7 @@ public class RentDAO {
         }
     }
     /* Method to DELETE a house fro DB */
-    public void deleteHouse(Integer EmployeeID){
+    public void deleteHouse(RentEntryBean house){
         try{
             factory = new Configuration().configure().buildSessionFactory();
         }catch (Throwable ex) {
@@ -120,15 +123,16 @@ public class RentDAO {
             throw new ExceptionInInitializerError(ex);
         }
         Session session = factory.openSession();
-        Transaction tx = null;
         try{
-            tx = session.beginTransaction();
-            House employee =
-                    (House) session.get(House.class, EmployeeID);
-            session.delete(employee);
-            tx.commit();
+            session.beginTransaction();
+            //String hql = "delete from House where address = '" + house.getAddress() + "'";
+            //System.out.println(hql);
+            Query query = session.createQuery("delete from House where address = :address");
+            query.setParameter("address", house.getAddress());
+            query.executeUpdate();
+            session.getTransaction().commit();
         }catch (HibernateException e) {
-            if (tx!=null) tx.rollback();
+            System.err.println("!!!Failed to delete house!!! " + e);
             e.printStackTrace();
         }finally {
             session.close();
